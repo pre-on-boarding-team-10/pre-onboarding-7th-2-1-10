527f771daeb405e9610a92358f165797b29ff6d1
silviaoh <silvia.jisu.oh@gmail.com>
2022-11-02 15:07:42 +0900

    Fix: Eslint 에러
    - Router import 하지 않음

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
+import Router from './router';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
 
